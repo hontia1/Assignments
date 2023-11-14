1e4befa6a8ef797ddddfa79055ef2f5a861f5a18
hontia1 <hon_tia1@yahoo.co.uk>
2023-11-14 18:44:54 +0000

    My work

---
+++ index.js
@@ -0,0 +1,13 @@
+// document object model (innerHTML) 
+document.getElementById('first').innerHTML='Adedayo birthday is tomorrow Wednesday 15th November';
+document.getElementById ('first').style.color='red';
+document.getElementById('first').style.backgroundColor='green';
+document.getElementById ('first').style.fontSize='50px'
+document.write('Invite the whole gang to sign your card')
+window.alert('Happy Birthday')
+console.log('Wednesday 15th November')
+
+
+let You = 'Below is a list of items select your match'
+console.log(You)
+
